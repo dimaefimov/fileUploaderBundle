f0c5a13c058f11293e7e4678dd2d5c455d16d506
Unknown <dmitriy.efimov@yahoo.com>
2019-02-07 18:02:24 +0300

    xml config

--- DependencyInjection/FileUploaderExtension.php
+++ DependencyInjection/FileUploaderExtension.php
@@ -10,11 +10,11 @@ class FileUploaderExtension extends Extension
 {
   public function load(array $configs, ContainerBuilder $container)
   {
-    $loader = new YamlFileLoader(
+    $loader = new XmlFileLoader(
       $container,
       new FileLocator(__DIR__.'/../Resources/config')
     );
-    $loader->load('services.yaml');
+    $loader->load('services.xml');
 
     $configuration = new Configuration();
     $config = $this->processConfiguration($configuration, $configs);
